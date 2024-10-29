6d967fdcf8e2120754a965898175b036283b4dc1
Celina <csarona@umich.edu>
2024-10-29 19:43:55 -0400

    Initial Commit

---
+++ scrapsIOS/scrapsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrapsIOSApp.swift
+//  scrapsIOS
+//
+//  Created by Celina Sarona on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct scrapsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scrapsIOSTests/scrapsIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  scrapsIOSTests.swift
+//  scrapsIOSTests
+//
+//  Created by Celina Sarona on 10/29/24.
+//
+
+import Testing
+@testable import scrapsIOS
+
+struct scrapsIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
